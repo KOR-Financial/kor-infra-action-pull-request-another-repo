7d3a0f565390bb6d28b83f9ea7adec6c5299a3b2
akopachevskyy <kopachevsky@gmail.com>
2021-08-17 21:43:17 +0300

    added title and message field for PR, fixes for token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-set -x
+#set -x
 
 if [ -z "$INPUT_SOURCE_FOLDER" ]
 then
@@ -23,17 +23,19 @@ else
 fi
 
 CLONE_DIR=$(mktemp -d)
-
+echo "env"
+env
 echo "Setting git variables"
 export GITHUB_TOKEN=$API_TOKEN_GITHUB
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 
 echo "Cloning destination git repository"
-git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone "https://$INPUT_USER_NAME:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+
 cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
 git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
@@ -46,11 +48,11 @@ then
   echo "Pushing git commit"
   git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
   echo "Creating a pull request"
-  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
-               -b $INPUT_DESTINATION_HEAD_BRANCH \
-               -B $INPUT_DESTINATION_BASE_BRANCH \
-               -H $INPUT_DESTINATION_HEAD_BRANCH \
-                  $PULL_REQUEST_REVIEWERS
+  gh pr create -t "$INPUT_TITLE" \
+               -b "$INPUT_COMMENT" \
+               -B "$INPUT_DESTINATION_BASE_BRANCH" \
+               -H "$INPUT_DESTINATION_HEAD_BRANCH"
+               #"$PULL_REQUEST_REVIEWERS"
 else
   echo "No changes detected"
 fi
